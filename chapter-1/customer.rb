5e0c40dc75c4161dcd060b89dd73cc95ea256366
Boris Paskhaver <boris.paskhaver@nyu.edu>
2018-06-20 01:16:55 -0400

    Finish Chapter 1 refactors

--- chapter-1/customer.rb
+++ chapter-1/customer.rb
@@ -11,19 +11,39 @@ class Customer
   end
 
   def statement
-    total_amount, frequent_renter_points = 0, 0
     result = "Rental record for #{@name}\n"
     @rentals.each do |element|
       frequent_renter_points += element.frequent_renter_points
-      
       # show figures for this rental
-      result += "\t" + element.movie.title + "\t" + element.charge + "\n"
-      total_amount += element.charge
+      result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
     end
 
     # add footer lines
-    result += "Amount owed is #{total_amount}\n"
-    result += "You earned #{frequent_renter_points} frequent renter points"
+    result += "Amount owed is #{total_charge}\n"
+    result += "You earned #{total_frequent_renter_points} frequent renter points"
     result
+  end
+
+  def html_statement
+    result = "<h1>Rentals for <em>#{@name}</em></h1><p>\n"
+    @rentals.each do |element|
+      # show figures for this rental
+      result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
+    end
+    # add footer lines
+    result += "<p>You owe<em>#{total_charge}</em></p>\n"
+    result += "On this rental you earned " +
+           "<em>#{total_frequent_renter_points}</em> " +
+           "frequent renter points<p>"
+  end
+
+  private
+
+  def total_charge
+    @rentals.inject(0) { |sum, rental| sum + rental.charge }
+  end
+
+  def total_frequent_renter_points
+    @rentals.inject(0) { |sum, rental| sum + rental.frequent_renter_points }
   end
 end
--- chapter-1/movie.rb
+++ chapter-1/movie.rb
@@ -4,9 +4,54 @@ class Movie
   CHILDRENS = 2
 
   attr_reader :title
+  attr_writer :price
   attr_accessor :price_code
 
-  def initialize(title, price_code)
-    @title, @price_code = title, price_code
+  def price_code=(value)
+    @price_code = value
+  end
+
+  def initialize(title, the_price_code)
+    @title, self.price_code = title, the_price_code
+  end
+
+  def charge(days_rented)
+    @price.charge(days_rented)
+  end
+
+  def frequent_renter_points(days_rented)
+    @price.frequent_renter_points(days_rented)
+  end
+end
+
+module DefaultPrice
+  def frequent_renter_points(days_rented)
+    1
+  end
+end
+
+class RegularPrice
+  include DefaultPrice
+
+  def charge(days_rented)
+    result = 2
+    result += (days_rented - 2) * 1.5 if days_rented > 2
+    result
+  end
+end
+
+class NewReleasePrice
+  def frequent_renter_points(days_rented)
+    days_rented > 1 ? 2 : 1
+  end
+end
+
+class ChildrensPrice
+  include DefaultPrice
+
+  def charge(days_rented)
+    result = 1.5
+    result += days_rented - 3) * 1.5 if days_rented > 3
+    result
   end
 end
